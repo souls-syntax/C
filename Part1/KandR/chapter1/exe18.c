2f79bfc2a353e5d96e312a02e86e28e7104c5218
Souls Syntax Nix <souls.syntax@gmail.com>
2025-11-10 22:16:31 +0530

    exec17

---
+++ Part1/KandR/chapter1/exe18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int getlinee(char line[], int maxline);
+
+int main(void) {
+  char line[MAXLINE];
+
+  while((len = getlinee(line, MAXLINE)) > 0) {
+
+  }
+
+}
